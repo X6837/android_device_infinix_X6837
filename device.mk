94e832126fe44e9359cb9112a65c301b25e40348
Woomymy <woomy@woomy.be>
2023-02-27 19:47:19 +0100

    X6837: Install fstab.mt6789 into first_stage_ramdisk
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I6a0a98dcd8e5ff73dc8a934d1ea9fe43fd5107ca

--- device.mk
+++ device.mk
@@ -42,6 +42,10 @@ PRODUCT_PACKAGES += \
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Init files
+PRODUCT_PACKAGES += \
+	fstab.mt6789_vendor_ramdisk
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor:64
