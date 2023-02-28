3e511f6c8876e673ba4c886608da843f6ed91413
Woomymy <woomy@woomy.be>
2023-02-28 13:58:05 +0100

    X6837: Build MTK boot control hal V1.2 in recovery
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I0a8864bc326e83b63de346f685e1a651abb0a1e1

--- device.mk
+++ device.mk
@@ -39,6 +39,10 @@ PRODUCT_PACKAGES += \
     otapreopt_script \
     checkpoint_gc
 
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-mtkimpl.recovery
+
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
@@ -69,7 +73,8 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/mediatek
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/infinix/X6837/X6837-vendor.mk)
