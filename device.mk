ce08629b78687b1c2517a464c1afe2280702dfda
Woomymy <woomy@woomy.be>
2023-04-06 21:23:55 +0200

    X6837: Build OSS MediaTek Thermal HAL
    * Configuration strictly matches stock HAL configuration
    
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I1edafd1c2a311fb2a5e6231eece48e3629f099ce
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -468,10 +468,11 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/transsion
 
 # Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@2.0.vendor
-    
+$(call inherit-product, hardware/mediatek/hidl/thermal/device.mk)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
