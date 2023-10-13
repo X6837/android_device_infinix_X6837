33446e1413446a45a3a19147087c43deba7e1c15
xoo2001 <ekora2001@gmail.com>
2023-10-14 01:44:49 +0700

    X6837: Initial skeleton tree

---
+++ device.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/infinix/X6837
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/infinix/X6837/X6837-vendor.mk)
