f211ae4c97aa1b67008e34c310a50b23dc864f34
fjrXTR <fajarslebew31@gmail.com>
2024-07-07 18:41:55 +0000

    X6837: Link Older VNDK required libs on `camerahalserver`
    Change-Id: I1007609c4072e8c96b4560f23e892322526b312b
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -74,9 +74,9 @@ function blob_fixup {
             "${PATCHELF}" --add-needed "libshim_sensors.so" "${2}"
             ;;
         vendor/bin/hw/mt6789/camerahalserver)
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
-            "${PATCHELF}" --replace-needed "libbinder.so" "libbinder-v32.so" "${2}"
-            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v31.so" "${2}"
+            "${PATCHELF}" --replace-needed "libbinder.so" "libbinder-v31.so" "${2}"
+            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v31.so" "${2}"
             ;;
         vendor/etc/init/android.hardware.media.c2@1.2-mediatek.rc)
             sed -i 's/@1.2-mediatek/@1.2-mediatek-64b/g' "${2}"
