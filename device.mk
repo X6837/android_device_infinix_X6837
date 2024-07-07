c10beb348048364be702e7e8e38a28a0cb6d7825
fjrXTR <fajarslebew31@gmail.com>
2024-07-07 22:50:42 +0000

    X6837: Fix missing separator & Drop prebuilt power-service
    Change-Id: I2b874e7eed4679321d4fd7cb8e5eb55f2abb9065
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -196,7 +196,7 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.mt6789 \
-    android.hardware.health-service.mt6789-recovery
+    android.hardware.health-service.mt6789-recovery \
     charger_res_images_vendor
 
 # HIDL
